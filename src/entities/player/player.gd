fcddc1d61b27653b86b8b139cb137cfd6846b385
idkiftheothernamesaretaken <mardlion12@gmail.com>
2026-02-16 10:50:27 +0300

    Добавил текстуру белых плиток.

--- src/entities/player/player.gd
+++ src/entities/player/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 # --- Настройки ---
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+var killed = 0
 # --- Компоненты ---
 @export var health_component: HealthComponent
 # HitboxComponent (Меч) - нужно добавить в сцену игрока
--- src/screens/clickerclick.gd
+++ src/screens/clickerclick.gd
@@ -1,11 +1,11 @@
 extends Label
-@export var stats: Dictionary
-var player = get_tree().get_first_node_in_group("player")
+var a = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
+func _on_button_3_button_down() -> void:
+	a += 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float):
-	text = "Kills: " + str(player.killed)
+	text = str(a)
